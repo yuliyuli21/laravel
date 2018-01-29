4fca5a48374d529c37c897917ce966bce8cbc018
woowli <869267211@qq.com>
2018-01-29 17:14:30 +0800

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -136,4 +136,13 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 
-
+Route::resource('users', 'UsersController');
+/*
+Route::get('/users', 'UsersController@index')->name('users.index');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+*/
